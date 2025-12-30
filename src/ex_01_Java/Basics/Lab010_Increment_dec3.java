21a1e3334e6adeeaf316eb6e1e3509e5449ae4fd
Hemant9813 <hemant9813@gmail.com>
2025-12-30 22:44:49 +0530

    New practice

---
+++ src/ex_01_Java/Basics/Lab010_Increment_dec3.java
@@ -0,0 +1,14 @@
+package ex_01_Java.Basics;
+
+public class Lab010_Increment_dec3 {
+    public static void main(String[] args){
+
+        int a = 5;
+
+        System.out.println(a++ + a--);
+
+        //A->  a++   5
+        //B->  a--   6
+        //a=A+B   5+6
+    }
+}
